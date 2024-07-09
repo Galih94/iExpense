8cfe162d1c18642f27c81e13c5d6401ebec9b292
galih <samudragalih94@gmail.com>
2024-07-09 10:40:44 +0700

    feat(model): create model with `@Observable` container

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -5,7 +5,19 @@
 //  Created by Galih Samudra on 08/07/24.
 //
 
+import Observation
 import SwiftUI
+
+struct ExpenseItem {
+    let name: String
+    let type: String
+    let amount: Double
+}
+
+@Observable
+class Expenses {
+    var _items = [ExpenseItem]()
+}
 
 struct ContentView: View {
     var body: some View {
