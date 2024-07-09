ba91d6c4ddb029e54102636af1e0f666b684b450
galih <samudragalih94@gmail.com>
2024-07-09 12:59:00 +0700

    feat(AddView): create `AddView` file to create new expenses

---
+++ iExpense/iExpense/AddExpense/AddView.swift
@@ -0,0 +1,35 @@
+//
+//  AddView.swift
+//  iExpense
+//
+//  Created by Galih Samudra on 09/07/24.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    @State private var _name: String = ""
+    @State private var _type: String = "Personal"
+    @State private var _amount: Double = 0.0
+    
+    let types = ["Personal", "Business"]
+    var body: some View {
+        NavigationStack {
+            Form {
+                TextField("Name", text: $_name)
+                Picker("Type", selection: $_type) {
+                    ForEach(types, id: \.self) {
+                        Text($0)
+                    }
+                }
+                TextField("Amount", value: $_amount, format: .currency(code: "IDR"))
+                    .keyboardType(.decimalPad)
+            }
+            .navigationTitle("Add New Expense")
+        }
+    }
+}
+
+#Preview {
+    AddView()
+}
